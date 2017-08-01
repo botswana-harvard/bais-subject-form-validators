efb3fca0a3133f94aa22fca275dd68f5884e8fd7
aseokana <aseokana@bhp.org.bw>
2017-08-01 17:18:41 +0200

    testing taken_tobacco: YES, condition

--- bais_subject_form_validators/tests/test_section2_form_validator.py
+++ bais_subject_form_validators/tests/test_section2_form_validator.py
@@ -50,3 +50,45 @@ class TestSection2FormValidator(TestCase):
         self.assertIn('substance_frequency_alcohol',
                       form_validator._errors)
         self.assertIn(REQUIRED_ERROR, form_validator._error_codes)
+
+    def test_substance_method_tobacco(self):
+        options = {
+            'taken_tobacco': YES,
+            'substance_method_tobacco': None}
+        form_validator = Section2FormValidator(cleaned_data=options)
+        try:
+            form_validator.validate()
+        except forms.ValidationError:
+            pass
+        self.assertIn('substance_method_tobacco',
+                      form_validator._errors)
+        self.assertIn(REQUIRED_ERROR, form_validator._error_codes)
+
+    def test_substance_age_tobacco(self):
+        options = {
+            'taken_tobacco': YES,
+            'substance_method_tobacco': 'Smoking',
+            'substance_age_tobacco': None}
+        form_validator = Section2FormValidator(cleaned_data=options)
+        try:
+            form_validator.validate()
+        except forms.ValidationError:
+            pass
+        self.assertIn('substance_age_tobacco',
+                      form_validator._errors)
+        self.assertIn(REQUIRED_ERROR, form_validator._error_codes)
+
+    def test_substance_frequency_tobacco(self):
+        options = {
+            'taken_tobacco': YES,
+            'substance_method_tobacco': 'Smoking',
+            'substance_age_tobacco': 13,
+            'substance_frequency_tobacco': None}
+        form_validator = Section2FormValidator(cleaned_data=options)
+        try:
+            form_validator.validate()
+        except forms.ValidationError:
+            pass
+        self.assertIn('substance_frequency_tobacco',
+                      form_validator._errors)
+        self.assertIn(REQUIRED_ERROR, form_validator._error_codes)
