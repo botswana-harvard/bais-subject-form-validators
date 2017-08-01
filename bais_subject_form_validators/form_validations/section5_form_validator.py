d66003ebed9c5fc19b135d5b84502492b6a0b2a1
Kwasi Darkwa <kwasidarkwa@digawana.bhp.org.bw>
2017-08-01 14:58:31 +0200

    sct5

---
+++ bais_subject_form_validators/form_validations/section5_form_validator.py
@@ -0,0 +1,13 @@
+from edc_base.modelform_validators import FormValidator
+from edc_constants.constants import OTHER, YES, NEVER, NO
+
+
+class Section5FormValidator(FormValidator):
+
+    def clean(self):
+
+        self.required_if(
+            OTHER,
+            field='tb_information_source',
+            field_required='tb_information_source_other',
+        )
