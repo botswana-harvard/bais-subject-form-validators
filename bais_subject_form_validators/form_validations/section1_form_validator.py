9b03492c940c9ec36687ce155dd0a4eecb52624f
aseokana <aseokana@bhp.org.bw>
2017-08-01 10:47:21 +0200

    form validators for section1

--- bais_subject_form_validators/form_validations/section1_form_validator.py
+++ bais_subject_form_validators/form_validations/section1_form_validator.py
@@ -1,5 +1,5 @@
 from edc_base.modelform_validators import FormValidator
-from edc_constants.constants import OTHER
+from edc_constants.constants import OTHER, YES, NEVER, NO
 
 
 class Section1FormValidator(FormValidator):
@@ -10,6 +10,54 @@ class Section1FormValidator(FormValidator):
             OTHER,
             field='respondent_employment',
             field_required='respondent_employment_other',
+        )
+
+        self.required_if(
+            YES,
+            field='mine',
+            field_required='mine_period'
+        )
+
+        self.required_if(
+            OTHER,
+            field='mine_occupation',
+            field_required='mine_occupation_other',
+        )
+
+        self.required_if(
+            YES,
+            field='mine',
+            field_required='commodity'
+        )
+
+        self.required_if(
+            OTHER,
+            field='commodity',
+            field_required='commodity_other',
+        )
+
+        self.required_if(
+            OTHER,
+            field='religion',
+            field_required='religion_other',
+        )
+
+        self.not_required_if(
+            NEVER,
+            field='marital_status',
+            field_required='respondent_marriage_age',
+        )
+
+        self.required_if(
+            NO,
+            field='living_with_spouse',
+            field_required='spouse_visit',
+        )
+
+        self.not_required_if(
+            NEVER,
+            field='marital_status',
+            field_required='respondent_married_years',
         )
 
         return self.cleaned_data
