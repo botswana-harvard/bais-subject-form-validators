9729aaced3e344991929dc4b83e89b79726094c0
aseokana <aseokana@bhp.org.bw>
2017-08-07 08:58:26 +0200

    form validators for household_questionnaire

--- bais_subject_form_validators/form_validations/household_questionnaire_form_validator.py
+++ bais_subject_form_validators/form_validations/household_questionnaire_form_validator.py
@@ -1,5 +1,7 @@
 from edc_base.modelform_validators import FormValidator
-from edc_constants.constants import OTHER, YES
+from edc_constants.constants import OTHER, YES, NO
+
+from ..constants import YES_LEFT
 
 
 class HouseholdQuestionnaireFormValidator(FormValidator):
@@ -14,8 +16,38 @@ class HouseholdQuestionnaireFormValidator(FormValidator):
 
         self.required_if(
             YES,
-            field='person_citizenship',
-            field_required='person_citizenship_other',
+            field='person_biological_mother_alive',
+            field_required='person_biological_mother_household',
+        )
+
+        self.required_if(
+            YES,
+            field='person_biological_father_alive',
+            field_required='person_biological_father_household',
+        )
+
+        self.not_required_if(
+            NO,
+            field='person_attended_school',
+            field_required='person_currently_studying',
+        )
+
+        self.not_required_if(
+            YES_LEFT,
+            field='person_attended_school',
+            field_required='person_currently_studying',
+        )
+
+        self.required_if(
+            YES,
+            field='person_work_unpaid',
+            field_required='person_work_unpaid_reason',
+        )
+
+        self.required_if(
+            OTHER,
+            field='person_work_unpaid_reason',
+            field_required='person_work_unpaid_reason_other',
         )
 
         return self.cleaned_data
